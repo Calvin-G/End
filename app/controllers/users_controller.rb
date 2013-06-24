6ed375addd2fd60f034be4d2d8546ef18fcbc79a
Calvin Gedenk <calvin.gedenk@hotmail.com>
2013-06-24 08:06:03 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,19 @@ def new
   end
 
   def create
-    
+    @user = User.new(params[:user])
+	if @user.save
+		sign_in @user
+		flash[:succes] = "Welcome to the Sample App!"
+		redirect_to @user
+	else
+		render 'new'
+		end
+	end
   end
 
   def edit
-    
+    @user = User.find(params[:id])
   end
 
   def update
--- spec/requests/user_pages_spec.rb.rb
+++ spec/requests/user_pages_spec.rb.rb
@@ -10,8 +10,30 @@ describe "User pages" do
 
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
+	 describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+	  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
   end
-  .
-  .
-  .
+end
+  end
+ end
 end
